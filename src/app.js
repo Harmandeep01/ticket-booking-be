01f9a637c1dd426a45fc0a1044ba0a273264a8be
Harmandeep01 <sharmandeep@gmail.com>
2026-01-21 17:08:11 +0530

    changed baseurl

--- src/app.js
+++ src/app.js
@@ -1,28 +1,14 @@
-// src/app.js
-
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
 const corsOptions = require("./config/cors");
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ticket-booking-gb4gr29w0-harmandeep01s-projects.vercel.app",
-    ],
-    credentials: true,
-  })
-);
 
-app.use("/api/auth", require("./routes/auth_routes"));
-app.use("/api/events", require("./routes/event_routes"));
-app.use("/api/bookings", require("./routes/booking_routes"));
-
-// app.use(require("./middlewares/error"));
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/events", require("./routes/event.routes"));
+app.use("/api/bookings", require("./routes/booking.routes"));
 
 module.exports = app;
