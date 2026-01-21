27c7c04e032a3b1a5e229fded5391df8996fb888
Harmandeep01 <sharmandeep@gmail.com>
2026-01-21 17:48:19 +0530

    changed baseurl

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*", // allow all origins
+    origin: "https://ticket-booking-m7jcfj6jp-harmandeep01s-projects.vercel.app", // allow all origins
   })
 );
 
