815d82e2fb7fa6c49e6feb905821441fd47db23d
Harmandeep01 <sharmandeep@gmail.com>
2026-01-21 16:45:26 +0530

    changed script

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://ticket-booking-fe-five.vercel.app/",
+  baseURL: import.meta.env.VITE_API_URL || "https://ticket-booking-gb4gr29w0-harmandeep01s-projects.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
